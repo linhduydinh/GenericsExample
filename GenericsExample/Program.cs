d81762f40489fddb9b5623335666b22066a59e9c
LinhDinh <LinhDinh@DESKTOP-U36HJQS>
2019-11-06 14:27:24 +0700

    Test

---
+++ GenericsExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
